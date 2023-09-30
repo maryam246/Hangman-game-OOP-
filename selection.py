62c30d2e3f6275a784e00fa0b0de34a358450144
Maryam khanam <143519643+maryam246@users.noreply.github.com>
2023-09-30 20:19:18 +0500

    Add files via upload

---
+++ selection.py
@@ -0,0 +1,31 @@
+categories= {
+    "Easy": {
+        "Fruits": ["kiwi", "lemon", "mango", "orange", "peach", "pear", "pineapple", "plum",
+                   "raspberry", "strawberry", "watermelon", "blueberry", "blackberry","apple",
+                   "banana", "cherry", "date", "elderberry", "fig", "grape"],
+        "Foods": ["sushi", "taco", "Kheer", "fried chicken", "pasta", "steak", "sushi", "ramen",
+                  "ice cream", "pancake","sandwich", "soup", "french fries", "chocolate", "salad",
+                  "sushi", "cheeseburger","pizza", "burger", "spaghetti","bryani"],
+        "Colors": [ "orange", "gray", "violet", "maroon", "indigo", "olive","black", "lavender",
+                    "magenta","peach", "silver","blue", "green", "red", "yellow", "purple", "pink", "brown"]
+    },
+    "Medium": {
+        "Animals": ["zebra", "koala", "penguin", "cheetah", "elephant seal", "lemur", "kangaroo", "hippopotamus",
+                    "sloth", "rhinoceros", "gazelle", "panda","lion", "tiger", "elephant", "dog", "cat", "rabbit", "giraffe"],
+        "Sports": ["football", "cricket", "golf", "swimming", "boxing", "badminton", "hockey", "rugby", "table tennis", "cycling",
+                   "skiing", "surfing", "wrestling", "karate", "gymnastics","soccer", "basketball", "tennis", "volleyball", "baseball"],
+        "Cities": [ "mumbai", "beijing", "istanbul", "dubai", "singapore", "bangkok", "shanghai", "seoul",
+                    "karachi","delhi", "jakarta", "hongkong","newyork", "paris", "london", "tokyo", "sydney",
+                    "chicago", "toronto","singapore", "bangkok", "shanghai"]
+    },
+    "Hard": {
+        "Countries": [ "united kingdom", "italy", "japan", "china", "brazil", "argentina", "mexico", "russia", "south africa",
+                       "egypt", "nigeria", "kenya", "morocco", "sweden", "norway", "denmark", "ireland", "portugal","greece",
+                       "turkey", "uae", "saudi arabia", "pakistan", "south korea", "thailand", "indonesia","usa", "canada",
+                       "india", "australia", "france", "germany", "spain"],
+        "Birds": ["parrot", "robin", "owl", "seagull", "dove", "swan", "peacock", "crow", "canary", "vulture", "pelican",
+                  "blue jay", "woodpecker", "turkey", "ostrich","sparrow", "eagle", "hawk", "flamingo", "penguin"],
+        "Movies": ["The Dark Knight","Jurassic Park","3 Idiots","PK","Dangal","Bahubali","avatar", "titanic",
+                   "inception", "jaws","gladiator","Wrong No.","bol","Actor in Law","Teefa in Trouble","Parwaaz Hai Junoon"]
+    }
+}
